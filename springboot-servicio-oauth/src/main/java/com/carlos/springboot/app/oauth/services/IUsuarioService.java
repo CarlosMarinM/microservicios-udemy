62f3450a3e29eda703a8eb3000e0c7f92bc4208f
carlos.marinm@hotmail.com <carlos.marinm@hotmail.com>
2020-01-14 21:08:16 -0500

    configuracion OAuth

---
+++ springboot-servicio-oauth/src/main/java/com/carlos/springboot/app/oauth/services/IUsuarioService.java
@@ -0,0 +1,8 @@
+package com.carlos.springboot.app.oauth.services;
+
+import com.carlos.springboot.app.usuarios.commons.models.entity.Usuario;
+
+public interface IUsuarioService {
+
+	public Usuario findByUsername(String username);
+}
